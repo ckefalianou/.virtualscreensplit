bf8f8e8b3082f2fd63ba82aa52833d025fb1b239
Christine Kefalianou <45734845+ckefalianou@users.noreply.github.com>
2025-02-21 17:09:55 +0200

    Update virtualscreensplit.sh

--- virtualscreensplit.sh
+++ virtualscreensplit.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # Get the primary display name
-PRIMARY_DISPLAY=$(xrandr | grep " connected" | awk '{print $1}')
+PRIMARY_DISPLAY="DP-1"
 SECOND_DISPLAY="HDMI-1" # Change this if your second monitor has a different name
 
-# Get the screen resolution
-SCREEN_RESOLUTION=$(xrandr | grep "*" | awk '{print $1}')
-WIDTH=$(echo $SCREEN_RESOLUTION | cut -d 'x' -f1)
-HEIGHT=$(echo $SCREEN_RESOLUTION | cut -d 'x' -f2)
+PRIMARY_RESOLUTION=$(xrandr --current | grep -A1 "$PRIMARY_DISPLAY" | tail -n 1 | awk '{print $1}')
+PRIMARY_WIDTH=$(echo $PRIMARY_RESOLUTION | cut -d 'x' -f 1) #${PRIMARY_WIDTH}
+PRIMARY_HEIGHT=$(echo $PRIMARY_RESOLUTION | cut -d 'x' -f 2) #${PRIMARY_HEIGHT}
+HALF_WIDTH=$((PRIMARY_WIDTH / 2)) #${HALF_WIDTH}
+MIDDLE_WIDTH=$(( 1920 + 0 ))
+MIDDLE_THIRDS=$(( (PRIMARY_WIDTH - MIDDLE_WIDTH) / 2 ))
+THIRD_SCREEN_OFFSET=$((PRIMARY_WIDTH - MIDDLE_THIRDS))
 
-# Function to remove existing virtual monitors safely
 remove_virtual_monitors() {
     xrandr --delmonitor VIRTUAL1 2>/dev/null
     xrandr --delmonitor VIRTUAL2 2>/dev/null
@@ -18,29 +20,26 @@ remove_virtual_monitors() {
 
 # Function to set single ultrawide mode
 single_monitor() {
+    xrandr --output $PRIMARY_DISPLAY --mode ${PRIMARY_WIDTH}x${PRIMARY_HEIGHT} --primary --output $SECOND_DISPLAY --off
     remove_virtual_monitors
-    xrandr --output $PRIMARY_DISPLAY --mode ${WIDTH}x${HEIGHT} --primary --output $SECOND_DISPLAY --off
-    echo "Switched to Single Ultrawide Monitor (${WIDTH}x${HEIGHT})"
+    echo "Switched to Single Ultrawide Monitor (${PRIMARY_WIDTH}x${PRIMARY_HEIGHT})"
 }
 
 # Function to split ultrawide into two virtual monitors
 virtual_split() {
     remove_virtual_monitors
-    HALF_WIDTH=$((WIDTH / 2))
-    xrandr --setmonitor VIRTUAL1 ${HALF_WIDTH}/${WIDTH}x${HEIGHT}/${HEIGHT}+0+0 $PRIMARY_DISPLAY
-    xrandr --setmonitor VIRTUAL2 ${HALF_WIDTH}/${WIDTH}x${HEIGHT}/${HEIGHT}+${HALF_WIDTH}+0 none
-    echo "Switched to Virtual Split Mode (2x ${HALF_WIDTH}x${HEIGHT})"
+    xrandr --setmonitor VIRTUAL1 ${HALF_WIDTH}/${PRIMARY_WIDTH}x${PRIMARY_HEIGHT}/${PRIMARY_HEIGHT}+0+0 $PRIMARY_DISPLAY
+    xrandr --setmonitor VIRTUAL2 ${HALF_WIDTH}/${PRIMARY_WIDTH}x${PRIMARY_HEIGHT}/${PRIMARY_HEIGHT}+${HALF_WIDTH}+0 none
+    echo "Switched to Virtual Split Mode (2x ${HALF_WIDTH}x${PRIMARY_HEIGHT})"
 }
 
 # Function to split ultrawide into three virtual monitors
 virtual_three_split() {
     remove_virtual_monitors
-    MID_WIDTH=1920
-    SIDE_WIDTH=$(((WIDTH - MID_WIDTH) / 2))
-    xrandr --setmonitor VIRTUAL1 ${SIDE_WIDTH}/${WIDTH}x${HEIGHT}/${HEIGHT}+0+0 $PRIMARY_DISPLAY
-    xrandr --setmonitor VIRTUAL2 ${MID_WIDTH}/${WIDTH}x${HEIGHT}/${HEIGHT}+${SIDE_WIDTH}+0 none
-    xrandr --setmonitor VIRTUAL3 ${SIDE_WIDTH}/${WIDTH}x${HEIGHT}/${HEIGHT}+$((${SIDE_WIDTH} + ${MID_WIDTH}))+0 none
-    echo "Switched to Virtual Three Split Mode (${SIDE_WIDTH}x${HEIGHT} | ${MID_WIDTH}x${HEIGHT} | ${SIDE_WIDTH}x${HEIGHT})"
+    xrandr --setmonitor VIRTUAL1 ${MIDDLE_THIRDS}/${PRIMARY_WIDTH}x${PRIMARY_HEIGHT}/${PRIMARY_HEIGHT}+0+0 $PRIMARY_DISPLAY
+    xrandr --setmonitor VIRTUAL2 ${MIDDLE_WIDTH}/${PRIMARY_WIDTH}x${PRIMARY_HEIGHT}/${PRIMARY_HEIGHT}+${MIDDLE_THIRDS}+0 none
+    xrandr --setmonitor VIRTUAL3 ${MIDDLE_THIRDS}/${PRIMARY_WIDTH}x${PRIMARY_HEIGHT}/${PRIMARY_HEIGHT}+${THIRD_SCREEN_OFFSET}+0 none
+    echo "Switched to Virtual Three Split Mode (${MIDDLE_THIRDS}x${PRIMARY_HEIGHT} | ${MIDDLE_WIDTH}x${PRIMARY_HEIGHT} | ${MIDDLE_THIRDS}x${PRIMARY_HEIGHT})"
 }
 
 # Function to enable physical dual monitors
@@ -52,9 +51,9 @@ physical_dual() {
 
 # Display menu
 echo "Choose a display mode:"
-echo "1) Single Ultrawide Monitor (${WIDTH}x${HEIGHT})"
-echo "2) Virtual Split Mode (2x Half Width x ${HEIGHT})"
-echo "3) Virtual Three Split Mode (Left | Center 1920px | Right)"
+echo "1) Single Ultrawide Monitor (${PRIMARY_WIDTH}x${PRIMARY_HEIGHT})"
+echo "2) Virtual Split Mode (2x ${HALF_WIDTH}x${PRIMARY_HEIGHT})"
+echo "3) Virtual Three Split Mode (${MIDDLE_THIRDS}x${PRIMARY_HEIGHT} | ${MIDDLE_WIDTH}x${PRIMARY_HEIGHT} | ${MIDDLE_THIRDS}x${PRIMARY_HEIGHT})"
 echo "4) Physical Dual Monitors"
 read -p "Enter your choice (1/2/3/4): " choice
 
